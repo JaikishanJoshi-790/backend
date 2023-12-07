de17df2e4cf1c9fd5c7bf110da7321c7c859241d
jaikishan joshi <jaikishanjoshi790@gmail.com>
2023-12-07 21:49:22 +0530

    utils code

---
+++ src/utils/ApiErrror.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+    constructor(
+        statuscode,
+        message ="something went wrong",
+        errors = [],
+        statck = ""
+    )
+    {
+        super(message)
+        this.statuscode = statuscode
+        this.data = null
+        this.message = message
+        this.scccess = false
+        this.errors = errors
+
+        if(statck){
+            this.stack = statck
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
